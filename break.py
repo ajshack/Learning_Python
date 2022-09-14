d30e72b61b439ea7366043c7d9bb0c37fa0cdcab
Jonee Shack <66149905+ajshack@users.noreply.github.com>
2022-09-13 21:35:15 -0500

    Add files via upload

---
+++ break.py
@@ -0,0 +1,19 @@
+# while True:
+# 	command = input("Type 'exit' to exit: ")
+# 	if (command  == exit):
+# 		break 
+
+# for x in range(1,101):
+#     print(x)
+#     if x == 3:
+#         break
+
+times = int(input("How many times do I have to tell you? "))
+
+for times in range(times):
+    print(("CLEAN YOUR ROOM"))
+    if times >= 3:
+        print("do you even listen anymore")
+        break
+
+    
