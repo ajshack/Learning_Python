0bef62c5434e0d23f1b62503d0b620950a48e4b9
Jonee Shack <66149905+ajshack@users.noreply.github.com>
2022-10-09 15:34:31 -0500

    Add files via upload

---
+++ list.py
@@ -0,0 +1,11 @@
+# colors = ["purple", "teal", "magenta", True, 8.9]
+
+# for color in colors: 
+#     print(color)
+
+# numbers = [4,6, 2, 9,10]
+# for num in numbers:
+#     print(num * num)
+
+
+
---
+++ list_While_Loop.py
@@ -0,0 +1,7 @@
+colors = ["purple", "teal", "magenta", "crimson", "emerald"]
+
+index = 0
+while index < len(colors):
+    print(f"{index}: {colors[index]} ")
+    index += 1
+
